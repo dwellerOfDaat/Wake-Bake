9646cf3d2d03b1f5eaae03eb0a90675c92fd860f
Demian <109124967+dwellerOfDaat@users.noreply.github.com>
2023-09-06 16:05:56 +0300

    adaptive of header

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
         const burgerIcon = e.target.closest('.burger-icon')
         const burgerNavLink = e.target.closest('.nav__link')
         if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 900) return
         if (burgerIcon) {
             e.preventDefault()
         }
