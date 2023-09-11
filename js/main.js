2437440a24a4a5b3fc24370af3723ab6fad800a0
Demian <109124967+dwellerOfDaat@users.noreply.github.com>
2023-09-11 21:17:41 +0300

    about section done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (function () {
     document.addEventListener('click', burgerInit)
+    
     function burgerInit(e) {
         const burgerIcon = e.target.closest('.burger-icon')
         const burgerNavLink = e.target.closest('.nav__link')
@@ -12,6 +13,24 @@
             document.body.classList.add('body--opened-menu')
         } else {
             document.body.classList.remove('body--opened-menu')
+        }
+    }
+
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-btn')
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+    
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+    
+    function closeModal(e) {
+        e.preventDefault()
+        const target = e.target
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
         }
     }
 })()
