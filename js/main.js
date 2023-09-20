06d7033b2969d031ac96a4271f4ddc9cbc71a8c8
Demian <109124967+dwellerOfDaat@users.noreply.github.com>
2023-09-20 16:18:51 +0300

    program section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 (function () {
     document.addEventListener('click', burgerInit)
-    
+
+    //----БУРГЕР----
+
     function burgerInit(e) {
         const burgerIcon = e.target.closest('.burger-icon')
         const burgerNavLink = e.target.closest('.nav__link')
@@ -16,21 +18,53 @@
         }
     }
 
+    //----МОДАЛКА----
+
     const modal = document.querySelector('.modal')
     const modalButton = document.querySelector('.about__img-btn')
     modalButton.addEventListener('click', openModal)
     modal.addEventListener('click', closeModal)
-    
+
     function openModal(e) {
         e.preventDefault()
         document.body.classList.toggle('body--opened-modal')
     }
-    
+
     function closeModal(e) {
         e.preventDefault()
         const target = e.target
         if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
             document.body.classList.remove('body--opened-modal')
         }
+    }
+    //----ТАБЫ----
+
+    const tabControls = document.querySelector('.tab-controls')
+
+    tabControls.addEventListener('click', toggleTab)
+
+    function toggleTab(e) {
+
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+        e.preventDefault()
+        if (tabControl.classList.contains('tab-controls__link--active')) return
+
+        const tabContentID = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentID)
+        const activeControl = document.querySelector('.tab-controls__link--active')
+        const activeContent = document.querySelector('.tab-content--show')
+
+        if (activeControl) {
+            activeControl.classList.remove('tab-controls__link--active')
+        }
+        if (activeContent) {
+            activeContent.classList.remove('tab-content--show')
+        }
+
+        tabControl.classList.add('tab-controls__link--active')
+        tabContent.classList.add('tab-content--show')
+
     }
 })()
