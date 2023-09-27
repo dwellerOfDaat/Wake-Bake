9081b831dacdb93e3a26b97af25994083fd75de7
Demian <109124967+dwellerOfDaat@users.noreply.github.com>
2023-09-27 19:24:39 +0300

    contact section final

--- js/main.js
+++ js/main.js
@@ -161,4 +161,11 @@
         }
 
     });
+
+    //----МАСКА ДЛЯ ТЕЛЕФОНА----
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
+
 })()
